024d65982c2e2987b5250f122b0b29a5d78ecbe5
TrucHuynh2002 <nguyentruchuynh@nl@gmail.com>
2022-09-20 13:36:41 +0700

    truchuynh

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,4 @@ Route::resource('congviec', CongviecController::class);
 Route::resource('chucvu', ChucvuController::class);
 Route::resource('congty', CongtyController::class);
 Route::resource('thuctapsinh', ThuctapsinhController::class);
-<<<<<<< HEAD
-// thay doi 3
-=======
-//thay đổi 123133131313
->>>>>>> 4d4e5e8b44daad3a8cb1093d3622e4f4cd1fe1bf
+
